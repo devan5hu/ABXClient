4cf9c949cc780a682ac20191df6f42749e8cfa16
Devanshu Chaudhari <devanshuchaudhari4@gmail.com>
2024-10-10 01:42:30 +0530

    added resendSeq request type

--- sol.cpp
+++ sol.cpp
@@ -1,14 +1,17 @@
-/* Author: devanshuC @Codeforces */
+/* Author : devanshuC @CodeForces */
 
-// Imports 
 #include <iostream>
 #include <string>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <queue>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
 
-//constants
+// Constants
 #define ZERO 0x0
 #define ONE 0x1
 #define TWO 0x2
@@ -16,47 +19,142 @@
 // Setup
 #pragma comment(lib, "Ws2_32.lib")
 
-void receiveData(SOCKET sockfd , std::queue < int > &missingPackets , int requestType) {
-    std::queue < int > q;
-    std::vector<char> buffer(INT_MAX);
+// Struct to hold packet information
+struct Packet {
+    std::string symbol;
+    char indicatorBuySell;
+    int quantity;
+    int price;
+    int packetSequence;
+
+    std::string toJson() const {
+        std::ostringstream oss;
+        oss << "\t{"
+            << "\"symbol\": \"" << symbol << "\", "
+            << "\"buySell\": \"" << indicatorBuySell << "\", "
+            << "\"quantity\": " << quantity << ", "
+            << "\"price\": " << price << ", "
+            << "\"packetSequence\": " << packetSequence
+            << "}";
+        return oss.str();
+    }
+};
+
+int handleSendError(SOCKET &ConnectSocket, addrinfo *result) {
+    printf("Error sending request\n");
+    closesocket(ConnectSocket);
+    freeaddrinfo(result);
+    return 1;
+}
+
+int sendData(SOCKET &ConnectSocket, const char *data, int length, addrinfo *result) {
+    int bytes_sent = send(ConnectSocket, data, length, 0);
+    
+    if (bytes_sent == -1) {
+        return handleSendError(ConnectSocket, result); 
+    }
+    
+    return bytes_sent;
+}
+
+bool isValidPacket(const Packet &packet) {
+    return !packet.symbol.empty() && 
+           (packet.indicatorBuySell == 'B' || packet.indicatorBuySell == 'S') && 
+           (packet.quantity > 0) && 
+           (packet.price >= 0) && 
+           (packet.packetSequence > 0);
+}
+
+void writeJsonToFile(std::vector<Packet> &data) {
+    std::ofstream outFile("packets.json"); // Open or create the JSON file
+    int dataSize = data.size(); 
+    if(dataSize == 0){
+        outFile.close();
+        return;
+    } 
+    if (!outFile) {
+        printf("Error creating JSON file!\n");
+        return;
+    }
+    outFile << "[\n"; 
+    for(int i=0; i<dataSize; i++){
+        if(i == dataSize-1) outFile << data[i].toJson() << '\n';
+        else outFile << data[i].toJson() << ',' << '\n';
+    }
+    outFile << "]"; 
+    outFile.close();
+    printf("Data written to packets.json\n");
+}
+
+void receiveData(SOCKET sockfd, std::queue<int> &missingPackets, int requestType, std::vector<Packet> &data) {
+    std::vector<char> buffer(1024);
     int bytesReceived;
     int packetSize = 17; // 4 + 1 + 4 + 4 + 4
     int sequence = 1;
-    while ((bytesReceived = recv(sockfd, buffer.data(), INT_MAX - 1, 0)) > 0) {
-        int totalReceived = 0;
 
+    while ((bytesReceived = recv(sockfd, buffer.data(), 1024, 0)) > 0) {
+        int totalReceived = 0;
         while (totalReceived < bytesReceived) {
             if (bytesReceived - totalReceived < packetSize) {
                 break;
             }
-            std::string symbol(buffer.data() + totalReceived, 4);  // symbol
-            char indicatorBuySell = buffer[totalReceived + 4];    // buysell 
-            int quantity =  ntohl(*(int*)(buffer.data() + totalReceived + 5)); // quantity
-            int price = ntohl(*(int*)(buffer.data() + totalReceived + 9));  // price
-            int packetSequence = ntohl(*(int*)(buffer.data() + totalReceived + 13)); // packetSequence
-            
-            // Total Packets Missed Logic            
-            int totalPacketsMissed = packetSequence - sequence;
-            if(totalPacketsMissed > 0){
-                for(int iter = 1; iter <= totalPacketsMissed; iter++){
-                    missingPackets.push(sequence - 1 + iter);
-                }
-            }
-            sequence = packetSequence;
-            sequence++;
-            
-            printf("Symbol: %.*s ", 4, symbol.c_str());
-            printf("Buy/Sell: %c " , indicatorBuySell);
-            printf("Quantity: %d " , quantity);
-            printf("Price: %d " , price);
-            printf("Packet Sequence: %d \n", packetSequence);
 
+            Packet packet;
+            packet.symbol = std::string(buffer.data() + totalReceived, 4);  // symbol
+            packet.indicatorBuySell = buffer[totalReceived + 4];    // buysell 
+            packet.quantity = ntohl(*(int*)(buffer.data() + totalReceived + 5)); // quantity
+            packet.price = ntohl(*(int*)(buffer.data() + totalReceived + 9));  // price
+            packet.packetSequence = ntohl(*(int*)(buffer.data() + totalReceived + 13)); // packetSequence
+            
             totalReceived += packetSize;
+
+            if(isValidPacket(packet)){
+                data.push_back(packet);
+            } else {
+                printf("Invalid packet received");
+            }
+
+            if (requestType == 1) {
+                int totalPacketsMissed = packet.packetSequence - sequence;
+                if (totalPacketsMissed > 0) {
+                    for (int iter = 0; iter < totalPacketsMissed; iter++) {
+                        missingPackets.push(sequence + iter);
+                    }
+                }
+                sequence = packet.packetSequence + 1;
+            }
+        }
+        if(requestType == 2){
+            break;
         }
     }
-    if (bytesReceived < 0) {
-        throw std::runtime_error("Error receiving data from socket.");
+    
+}
+
+bool isSocketValid(SOCKET socket) {
+    if (socket == INVALID_SOCKET) {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        return 0;
     }
+    return 1;
+}
+
+bool validateConnection(int iResult, SOCKET &connectSocket){
+    if (iResult == SOCKET_ERROR) {
+        printf("Unable to connect to server: %ld\n", WSAGetLastError());
+        closesocket(connectSocket);
+        connectSocket = INVALID_SOCKET;
+        WSACleanup();
+        return true; // Return true on error
+    }
+    printf("Connection established successfully.\n");
+    return false; // Return false on success
+}
+
+void sortData(std::vector <Packet> &data){
+    sort(data.begin(), data.end(), [](const Packet& a, const Packet& b){
+        return a.packetSequence < b.packetSequence;
+    });
 }
 
 int main() {
@@ -65,15 +163,14 @@ int main() {
     std::string api_endpoint = "/";
     std::string hostname = "127.0.0.1";
     std::string port = "3000";
-
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
         printf("WSAStartup failed: %d\n", iResult);
         return 1;
     }
 
-    struct addrinfo *result = NULL, hints;
+    struct addrinfo* result = NULL, hints;
 
     ZeroMemory(&hints, sizeof(hints));
     hints.ai_family = AF_INET;            // IPv4
@@ -88,77 +185,86 @@ int main() {
 
     SOCKET ConnectSocket = INVALID_SOCKET;
     ConnectSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (ConnectSocket == INVALID_SOCKET) {
-        printf("Error at socket(): %ld\n", WSAGetLastError());
+    if (!isSocketValid(ConnectSocket)) {
         freeaddrinfo(result);
         WSACleanup();
         return 1;
     }
 
     iResult = connect(ConnectSocket, result->ai_addr, (int)result->ai_addrlen);
-    if (iResult == SOCKET_ERROR) {
-        printf("Unable to connect to server: %ld\n", WSAGetLastError());
-        closesocket(ConnectSocket);
-        ConnectSocket = INVALID_SOCKET;
+    if(validateConnection(iResult, ConnectSocket)){
+        return 1;
+    }
+
+    printf("Connected to server.\n");
+    
+    std::queue<int> missingPackages; 
+    std::vector<Packet> data;
+    
+    char requestType1[2] = { ONE, ZERO };
+    int bytes_sent = send(ConnectSocket, requestType1, 2, 0);
+    printf("Request sent to server.\n");
+
+    receiveData(ConnectSocket, missingPackages, 1, data);
+
+    if (bytes_sent == -1) {
+        return handleSendError(ConnectSocket, result); 
+    }
+
+    ConnectSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (!isSocketValid(ConnectSocket)) {
+        freeaddrinfo(result);
         WSACleanup();
         return 1;
     }
 
-    printf("Connected to server. :) \n");
-    char requestType1[2] = {ONE , ZERO};
-    int bytes_sent = send(ConnectSocket, requestType1 , 2 , 0);
-    std::queue < int > missingPackages; 
-    receiveData(ConnectSocket , missingPackages, 2);
-    if (bytes_sent == -1) {
-        std::cerr << "Error sending request" << std::endl;
-        closesocket(ConnectSocket);
-        freeaddrinfo(result);
+    iResult = connect(ConnectSocket, result->ai_addr, (int)result->ai_addrlen);
+    if(validateConnection(iResult, ConnectSocket)){
         return 1;
     }
-    int i = 1;
-    int missed = -1;
-    while(!missingPackages.empty()){
-        printf("Here is the %dth missing package %d \n" , i , missingPackages.front());
-        if(missed == -1){
-            missed = missingPackages.front(); 
-        }
+
+    while (!missingPackages.empty()) {
+        char reqPacket = missingPackages.front();
+        // printf("Requested Packet: %d\n", missingPackages.front());
         missingPackages.pop();
-        i++;
-    }
+        char requestType2[2] = { TWO, reqPacket };
+        int bytes_sent2; 
+        try {
+            bytes_sent2 = send(ConnectSocket, requestType2, 2, 0);
+            // printf("Request for missing packet sent: %d\n", reqPacket);
 
-    char requestType2[2] = {TWO , 0x5};
-    int bytes_sent2; 
-    try{
-        printf("SENT 22222 \n");
-        bytes_sent2 = send(ConnectSocket, requestType2 , 2 , 0);
-        printf("SENT 22222 \n");
-    }
-    catch(std::runtime_error& e){
-        std::cerr << e.what() << '\n';
-    }
-    if(ConnectSocket != INVALID_SOCKET){
-        if (bytes_sent == -1) {
-            std::cerr << "Error sending request" << std::endl;
-            closesocket(ConnectSocket);
-            freeaddrinfo(result);
-            return 1;
+            if (bytes_sent == -1) {
+                return handleSendError(ConnectSocket, result);  
+            }
+        } catch (std::runtime_error& e) {
+            printf("Error sending request for missing packet\n");
         }
-        else{
-            try{
-                receiveData(ConnectSocket , missingPackages, 2);
+
+        if (ConnectSocket != INVALID_SOCKET) {
+            if (bytes_sent == -1) {
+                return handleSendError(ConnectSocket, result);  
             }
-            catch(std::runtime_error& e){
-                std::cerr << e.what() << "\n";
+            try {
+                receiveData(ConnectSocket, missingPackages, 2, data);
+            } catch (std::runtime_error& e) {
+                printf("Error receiving missing packet data\n");
             }
+        } else {
+            printf("Invalid Socket Connection\n");
         }
     }
-    else{
-        std::cerr << "INVALID SOCKET CONNECTION " << "\n";
-    }
 
+    printf("Data received successfully. Total packets received: %zu\n", data.size());
+
+    sortData(data);
+    
+    // Writing the JSON
+    writeJsonToFile(data);
+    
     freeaddrinfo(result);
     closesocket(ConnectSocket);
-    WSACleanup();
+    printf("Server Disconnected.\n");
 
+    // WSACleanup();
     return 0;
 }
